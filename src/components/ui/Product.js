f0850972d219160a4143335311549518f20588be
jesseseymour <jesse.seymour@catapultmarketing.com>
2018-06-05 13:24:23 -0400

    updated price spider structure

--- src/components/ui/Product.js
+++ src/components/ui/Product.js
@@ -1,4 +1,4 @@
-const Product = ({ name, subtitle, thumbnail, bvID, link, psID, formula }) =>
+const Product = ({ name, subtitle, thumbnail, bvID, link, psID, formula }) => 
   <div className="product">
     <a href={link}>
       <div className="img-container"><img src={thumbnail} /></div>
@@ -6,7 +6,7 @@ const Product = ({ name, subtitle, thumbnail, bvID, link, psID, formula }) =>
       <span className="pl-name">{name}</span>
       <div className="bv-container" id={`BVRRInlineRating-${bvID}`} data-iams-bvid={bvID}></div>
     </a>
-    <div className="pl-buynow"><div className="ps-widget" data-ps-sku={psID}><span className="ps-button-label">BUY NOW</span></div>{PriceSpider.insertWidget({"sku":psID})}</div>
+    <div className="pl-buynow"><div className="ps-widget" data-ps-sku={psID}></div></div>
   </div>
 
 
